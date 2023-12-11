b9e61d9b50346f6cafe4cff18002e99e916e82a0
Shadikul Islam <shadikul.islam@datasoft-bd.com>
2023-12-11 18:35:00 +0600

    File Added

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,7 +4,7 @@
     <div class="login">
         <input type="text" v-model="email" placeholder="Enter Email"/><br/>
         <input type="password" v-model="password" placeholder="Enter Password"/><br/>
-        <button v-on:click="Login">Login</button>
+        <button v-on:click="login">Login</button>
         <p>
             <router-link to = "/sign-up">Sign Up</router-link>
         </p>
--- src/components/services/LoginPage.js
+++ src/components/services/LoginPage.js
@@ -1,3 +1,30 @@
+import axios from "axios";
 export default {
-    name: "LoginPage"
-}
+    name: "LoginPage",
+    data() {
+        return {
+            email: "",
+            password: ""
+        };
+    },
+    methods: {
+        async login(){
+            console.log(this.email, this.password);
+            let result = await axios.get(
+                `http://localhost:3000/users?email=${this.email}&password=${this.password}`);
+            console.log(result);
+            if (result.status == 200 && result.data.length>0) {
+                localStorage.setItem("users-info", JSON.stringify(result.data));
+                this.$router.push({name: 'HomePage'})
+              }
+        }
+    },
+
+    mounted() {
+      let user = localStorage.getItem("users-info")
+      if(user)
+      {
+        this.$router.push({ name: 'HomePage' })
+      }
+    },
+};
